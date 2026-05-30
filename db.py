33debb92a94fe659519f999a81649beea00d6bec
PaolaCampana <146859224+PaolaCampana@users.noreply.github.com>
2026-05-29 17:57:16 -0700

    Update get_connection to use SSL configuration
    Refactor get_connection function to include SSL option.

--- db.py
+++ db.py
@@ -1,19 +1,12 @@
 import mysql.connector
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 def get_connection():
-
     return mysql.connector.connect(
-
-      os.getenv("DB_HOST")
-      os.getenv("DB_PORT")
-      os.getenv("DB_USER")
-      os.getenv("DB_PASSWORD")
-      os.getenv("DB_NAME")
-
-
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        ssl_disabled=False
     )
